2e252c305840e7b032835c0369d2cf2c3d4f4aef
dlansigan <dlansigan@berkeley.edu>
2016-06-03 21:51:59 -0700

    font size

--- assets/js/template.js
+++ assets/js/template.js
@@ -3,11 +3,11 @@ $(".expand-button").click(
     var activeDetails = $(this).attr("value");
     if( $(activeDetails).height() == 0 ){
       $(activeDetails).animate({ height: "40vh"}, 1000);
-      $(".pic-button-text").css("font-size", "1em");
+      $(".pic-button-text").css("font-size", "100%");
     }
     else{
       $(activeDetails).animate({ height: "0"}, 1000);
-      $(".pic-button-text").css("font-size", "1.5em");
+      $(".pic-button-text").css("font-size", "200%");
     }
     $(".details").not(activeDetails).animate({ height: "0"}, 1000);
   }
