8ffdebfa12f216bd2129c9bd53fa546ac3be418c
dlansigan <dlansigan@berkeley.edu>
2016-06-03 21:44:03 -0700

    fix formatting

--- assets/js/template.js
+++ assets/js/template.js
@@ -3,11 +3,11 @@ $(".expand-button").click(
     var activeDetails = $(this).attr("value");
     if( $(activeDetails).height() == 0 ){
       $(activeDetails).animate({ height: "40vh"}, 1000);
-      $(".pic-button-text").css("font-size", "1vw");
+      $(".pic-button-text").css("font-size", "1em");
     }
     else{
       $(activeDetails).animate({ height: "0"}, 1000);
-      $(".pic-button-text").css("font-size", "2vw");
+      $(".pic-button-text").css("font-size", "1.5em");
     }
     $(".details").not(activeDetails).animate({ height: "0"}, 1000);
   }
