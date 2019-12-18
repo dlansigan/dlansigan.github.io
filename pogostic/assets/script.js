6980f9ac12b60a98b7e662a77d75323224cc025e
Dana <dlol@stanford.edu>
2019-12-18 11:23:42 -0800

    Update css for tables

--- pogostic/assets/script.js
+++ pogostic/assets/script.js
@@ -160,7 +160,7 @@ function fillDefTable(tableID,pkmnList,pkmnTypeList,bestPkmn,bestPkmnType){
       var cell = row.insertCell(0);
     }
 
-    cell.innerHTML = pkmnList[bestPkmn[i]]+'<BR>'+pkmnTypeList[bestPkmnType[i]];
+    cell.innerHTML = pkmnList[bestPkmn[i]]+'<BR><i>'+pkmnTypeList[bestPkmnType[i]]+'</i>';
     }
 }
 
